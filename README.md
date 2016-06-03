a38f0efab11b4c5b31c09fafb6f4944ddffd41a5
Christopher Chen <www.chrischen@gmail.com>
2016-06-03 13:38:20 -0700

    asdf

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # RickrollScript
+usage: ./test.sh
---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#sudo osascript -e "set Volume 10"
+open https://www.youtube.com/watch?v=dQw4w9WgXcQ
+open -a 'App Store'
+
+/usr/bin/osascript -e "tell application \"Google Chrome\"" -e "activate" -e "make new window" -e "tell application \"System Events\"" -e "keystroke \"f\" using {control down, command down}" -e "end tell" -e "end tell"
+sleep 2
+/usr/bin/osascript -e "tell application \"Finder\"" -e "set desktopSize to bounds of window of desktop" -e "end tell" -e "tell application \"Safari\"" -e "activate" -e "set bounds of window 1 to desktopSize" -e "end tell"
+sleep 5
+/usr/bin/osascript -e "tell application \"Google Chrome\"" -e "activate" -e "make new window" -e "tell application \"System Events\"" -e "keystroke \"f\" using {control down, command down}" -e "end tell" -e "end tell"
